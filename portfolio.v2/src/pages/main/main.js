246c455d69b89b204649f5c322306b6a442e9c16
Angel <angel.l.puente@gmail.com>
2023-11-19 20:54:23 -0600

    projects link styled

--- portfolio.v2/src/pages/main/main.js
+++ portfolio.v2/src/pages/main/main.js
@@ -1,23 +1,34 @@
 import "./main.css";
-import face from '../../photos/meHead.png'
+import face from "../../photos/meHead.png";
 
 export default function Main() {
   return (
     <div className="main">
       <div className="introText">
-        <h1>Angel <span className="coloredText">Puente</span></h1>
-        <h3>Fullstack Engineer | Designer | Tech-<span className="coloredText">lover</span></h3>
-        <h3>[hello <span className="coloredText">there</span>]</h3>
+        <h1>
+          Angel <span className="coloredText">Puente</span>
+        </h1>
+        <h3>
+          Fullstack Engineer | Designer | Tech-
+          <span className="coloredText">lover</span>
+        </h3>
+        <h3>
+          [hello <span className="coloredText">there</span>]
+        </h3>
       </div>
       <div className="mainContainer">
         <div class="navContainer">
           <div className="me">
             <div className="profilePicture">
-                <img src={face} />
+              <img src={face} />
             </div>
           </div>
-          <div className="about"></div>
-          <div className="projects"></div>
+          <div className="projects">
+            <a>
+              <h1 className="projectsLink">Projects</h1>
+            </a>
+          </div>
+          <div className="contact"></div>
         </div>
       </div>
     </div>
