2a1eb357cf0022ee57ad54f6da3f5a805935830d
Angel <angel.l.puente@gmail.com>
2023-11-20 14:31:19 -0600

    switch cases and state controls for projects

--- portfolio.v2/src/pages/main/main.js
+++ portfolio.v2/src/pages/main/main.js
@@ -2,43 +2,60 @@ import "./main.css";
 import Nav from "../../components/nav/nav";
 import face from "../../images/meHead.png";
 
+const svgArray = [
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-plain.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/django/django-plain.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/python/python-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/heroku/heroku-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+];
+
 export default function Main() {
   return (
     <>
-    <Nav/>
-    <div className="main">
-      <div className="introText">
-        <h1>
-          Angel <span className="coloredText">Puente</span>
-        </h1>
-        <h3>
-          Fullstack Engineer | Designer | Tech-
-          <span className="coloredText">lover</span>
-        </h3>
-        <h3>
-          [<span className="coloredText">hello</span> there]
-        </h3>
-      </div>
-      <div className="mainContainer">
-        <div class="navContainer">
-          <div className="me">
-            <div className="profilePicture">
+      <Nav />
+      <div className="main">
+        <div className="introText">
+          <h1>
+            Angel <span className="coloredText">Puente</span>
+          </h1>
+          <h3>
+            Fullstack Engineer | Designer | Tech-
+            <span className="coloredText">lover</span>
+          </h3>
+          <h3>
+            [<span className="coloredText">hello</span> there]
+          </h3>
+        </div>
+        <div className="mainContainer">
+          <div class="navContainer">
+            <div className="me">
+              <div className="profilePicture">
                 <a href="/About">
-              <img src={face} />
+                  <img src={face} />
+                </a>
+              </div>
+            </div>
+            <div className="bottomScreens">
+              <a href="/Projects">
+                <h1 className="projectsLink">Projects</h1>
+              </a>
+            </div>
+            <div className="contact">
+              <a href="/Contact">
+                <h1>Contact</h1>
               </a>
             </div>
           </div>
-          <div className="bottomScreens">
-            <a href="/Projects">
-              <h1 className="projectsLink">Projects</h1>
-            </a>
-          </div>
-          <div className="contact">
-            <a href="/Contact"><h1>Contact</h1></a>
-          </div>
         </div>
       </div>
-    </div>
     </>
   );
 }
--- portfolio.v2/src/pages/projects/projects.js
+++ portfolio.v2/src/pages/projects/projects.js
@@ -1,16 +1,43 @@
 import "./projects.css";
-
+import { useState, useEffect } from "react";
 export default function Projects() {
+  const [currentProject, setCurrentProject] = useState(1);
+
+  const handleNextClick = (e) => {
+    e.preventDefault(e);
+    setCurrentProject(currentProject + 1);
+  };
+
+  const renderCurrentProject = () => {
+    switch (currentProject) {
+      case 1:
+        return <div>text</div>;
+      case 2:
+        return <div>text</div>;
+      case 3:
+        return <div>text</div>;
+    }
+  };
+
   return (
     <div className="projects">
       <div className="projectsBorder">
         <div className="projectsContainer">
           <div className="content1">
-            <p>Angel Puente
-
-I'm a Software Engineer with a knack for making code do cool things. I've danced my way through the ins and outs of React, JavaScript, and Node.js, turning lines of code into sleek applications that not only work but look good too. Beyond the coding grind, I'm into the whole puzzle-solving vibe of data structures—kind of like cracking a code mystery for fun.
-
-But it's not all about the code; I'm also the guy who makes sure your app looks snazzy. I've got a bag of tricks with various CSS frameworks, making sure everything is smooth, stylish, and user-friendly. In a nutshell, I'm your go-to person for turning tech jargon into awesome software that just works and looks pretty darn good doing it.</p>
+            <p>
+              Angel Puente I'm a Software Engineer with a knack for making code
+              do cool things. I've danced my way through the ins and outs of
+              React, JavaScript, and Node.js, turning lines of code into sleek
+              applications that not only work but look good too. Beyond the
+              coding grind, I'm into the whole puzzle-solving vibe of data
+              structures—kind of like cracking a code mystery for fun. But it's
+              not all about the code; I'm also the guy who makes sure your app
+              looks snazzy. I've got a bag of tricks with various CSS
+              frameworks, making sure everything is smooth, stylish, and
+              user-friendly. In a nutshell, I'm your go-to person for turning
+              tech jargon into awesome software that just works and looks pretty
+              darn good doing it.
+            </p>
           </div>
           <div className="content2"></div>
         </div>
