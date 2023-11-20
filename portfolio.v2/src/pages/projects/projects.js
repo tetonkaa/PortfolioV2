bc3b9861139ebeb83fcb294a2413aa0419d5a57c
Angel <angel.l.puente@gmail.com>
2023-11-20 16:32:54 -0600

    update projects

--- portfolio.v2/src/pages/projects/projects.js
+++ portfolio.v2/src/pages/projects/projects.js
@@ -1,4 +1,5 @@
 import "./projects.css";
+import Photo from '../../images/projectPhotos/d3fine.png'
 import { useState, useEffect } from "react";
 export default function Projects() {
   const [currentProject, setCurrentProject] = useState(1);
@@ -7,15 +8,45 @@ export default function Projects() {
     e.preventDefault(e);
     setCurrentProject(currentProject + 1);
   };
+  const handleBackClick = (e) => {
+    e.preventDefault();
+    setCurrentProject(currentProject - 1);
+  };
 
   const renderCurrentProject = () => {
     switch (currentProject) {
       case 1:
-        return <div>text</div>;
+        return (
+          <div>
+            <h1>Case 1</h1>
+            <img src={Photo}/>
+            <p>D3fine is a React based app that allows users to create customized personality assessments that can be shared to other users. Full CRUD technology with Firebase authentication embedded.</p>
+          </div>
+        );
       case 2:
-        return <div>text</div>;
+        return (
+          <div>
+            <h1>Case 2</h1>
+          </div>
+        );
       case 3:
-        return <div>text</div>;
+        return (
+          <div>
+            <h1>Case 2</h1>
+          </div>
+        );
+      case 4:
+        return (
+          <div>
+            <h1>Case 2</h1>
+          </div>
+        );
+      case 5:
+        return (
+          <div>
+            <h1>Case 2</h1>
+          </div>
+        );
     }
   };
 
@@ -23,23 +54,15 @@ export default function Projects() {
     <div className="projects">
       <div className="projectsBorder">
         <div className="projectsContainer">
-          <div className="content1">
-            <p>
-              Angel Puente I'm a Software Engineer with a knack for making code
-              do cool things. I've danced my way through the ins and outs of
-              React, JavaScript, and Node.js, turning lines of code into sleek
-              applications that not only work but look good too. Beyond the
-              coding grind, I'm into the whole puzzle-solving vibe of data
-              structures—kind of like cracking a code mystery for fun. But it's
-              not all about the code; I'm also the guy who makes sure your app
-              looks snazzy. I've got a bag of tricks with various CSS
-              frameworks, making sure everything is smooth, stylish, and
-              user-friendly. In a nutshell, I'm your go-to person for turning
-              tech jargon into awesome software that just works and looks pretty
-              darn good doing it.
-            </p>
+          <div className="currentProject">{renderCurrentProject()}</div>
+          <div className="projectButtons">
+            {currentProject < 5 && (
+              <button onClick={handleNextClick}>Next</button>
+            )}
+            {currentProject > 1 && (
+              <button onClick={handleBackClick}>BACK</button>
+            )}
           </div>
-          <div className="content2"></div>
         </div>
       </div>
     </div>
