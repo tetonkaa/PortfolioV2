c50f806d4769928a630ceda577f4d987751b840f
Angel <angel.l.puente@gmail.com>
2023-11-20 17:31:13 -0600

    update projects switch cases

--- portfolio.v2/src/pages/projects/projects.js
+++ portfolio.v2/src/pages/projects/projects.js
@@ -1,5 +1,8 @@
 import "./projects.css";
-import Photo from '../../images/projectPhotos/d3fine.png'
+import d3fine from "../../images/projectPhotos/d3fine.png";
+import ship from "../../images/projectPhotos/ShipVShip.png";
+import StopJockin from "../../images/projectPhotos/StopJockin.png";
+import SVGeezy from "../../images/projectPhotos/SVGeezy.png";
 import { useState, useEffect } from "react";
 export default function Projects() {
   const [currentProject, setCurrentProject] = useState(1);
@@ -19,32 +22,48 @@ export default function Projects() {
         return (
           <div>
             <h1>Case 1</h1>
-            <img src={Photo}/>
-            <p>D3fine is a React based app that allows users to create customized personality assessments that can be shared to other users. Full CRUD technology with Firebase authentication embedded.</p>
+            <img src={d3fine} />
+            <p>
+              D3fine is a React based app that allows users to create customized
+              personality assessments that can be shared to other users. Full
+              CRUD technology with Firebase authentication embedded.
+            </p>
           </div>
         );
       case 2:
         return (
           <div>
-            <h1>Case 2</h1>
+            <h1>Case 1</h1>
+            <img src={StopJockin} />
+            <p>
+              D3fine is a React based app that allows users to create customized
+              personality assessments that can be shared to other users. Full
+              CRUD technology with Firebase authentication embedded.
+            </p>
           </div>
         );
       case 3:
         return (
           <div>
-            <h1>Case 2</h1>
+            <h1>Case 1</h1>
+            <img src={SVGeezy} />
+            <p>
+              D3fine is a React based app that allows users to create customized
+              personality assessments that can be shared to other users. Full
+              CRUD technology with Firebase authentication embedded.
+            </p>
           </div>
         );
       case 4:
         return (
           <div>
-            <h1>Case 2</h1>
-          </div>
-        );
-      case 5:
-        return (
-          <div>
-            <h1>Case 2</h1>
+            <h1>Case 1</h1>
+            <img src={ship} />
+            <p>
+              D3fine is a React based app that allows users to create customized
+              personality assessments that can be shared to other users. Full
+              CRUD technology with Firebase authentication embedded.
+            </p>
           </div>
         );
     }
@@ -56,7 +75,7 @@ export default function Projects() {
         <div className="projectsContainer">
           <div className="currentProject">{renderCurrentProject()}</div>
           <div className="projectButtons">
-            {currentProject < 5 && (
+            {currentProject < 4 && (
               <button onClick={handleNextClick}>Next</button>
             )}
             {currentProject > 1 && (
